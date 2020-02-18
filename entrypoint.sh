cc851656a843273c864e6e9c2848c27e77cc3c14
b4b4r07 <b4b4r07@gmail.com>
2020-02-18 17:39:07 +0900

    Add match input

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,22 @@
 BASE="${INPUT_PATH:-.}"
 POLICY="${INPUT_POLICY:-policy}"
 FILES=( ${INPUT_FILES} )
+MATCHES=( ${INPUT_MATCHES} )
+
+match() {
+  local arg=${1}
+  if [[ ${#MATCHES[@]} == 0 ]]; then
+    return 0
+  fi
+  local match
+  for match in ${MATCHES[@]}
+  do
+    if [[ ${arg} == ${match} ]]; then
+      return 0
+    fi
+  done
+  return 1
+}
 
 run_conftest() {
   local file
@@ -10,11 +26,14 @@ run_conftest() {
 
   for file in "${FILES[@]}"
   do
+    match ${file} || continue
+
     ext="${file##*.}"
     conftest test \
       --no-color \
       --policy "${POLICY}" \
-      --input "${ext}" "${file}" || error=true
+      --input "${ext}" \
+      "${file}" || error=true
   done
 
   if ${error}; then
